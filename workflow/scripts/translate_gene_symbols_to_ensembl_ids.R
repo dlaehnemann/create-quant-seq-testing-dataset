a221c7dae3e5d80a47ae26c039efe653c313a108
dlaehnemann <david.laehnemann@hhu.de>
2024-01-22 15:55:54 +0100

    pass on parameters correctly

--- workflow/scripts/translate_gene_symbols_to_ensembl_ids.R
+++ workflow/scripts/translate_gene_symbols_to_ensembl_ids.R
@@ -24,7 +24,7 @@ while (class(mart)[[1]] != "Mart") {
       biomaRt::useEnsembl(
         biomart = "ENSEMBL_MART_ENSEMBL",
         dataset = str_c(snakemake@params[["species"]], "_gene_ensembl"),
-        version = snakemake@params[["version"]],
+        version = snakemake@params[["release"]],
         mirror = mart
       )
     },
